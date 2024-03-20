58209305ad7e1b53b70907af7c82dbd1fd9c32f4
AlexeyGoryachev <iwmoney11977@gmail.com>
2024-03-20 20:09:44 +0300

    task 2 is done.

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,34 @@ public class Main {
         System.out.println("Task 1.");
         int thisYear = 2024;
         checkYear(thisYear);
+        System.out.println("Task 2.");
+        int iOS = 0;
+        int android = 1;
+        int os = 0;
+        int clientDeviceYear = 2010;
+        selectingVersion(os, clientDeviceYear);
     }
     public static void checkYear(int year) {
         if ((year % 100 != 0 && year % 4 == 0) || (year % 400 == 0)) {
             System.out.println(year + " год является високосным");
         } else {
             System.out.println(year + " год не является високосным");
+        }
+    }
+    public static void selectingVersion(int os, int clientDeviceYear) {
+        if (os == 0) {
+            if (clientDeviceYear >= 2015) {
+                System.out.println("установите версию приложения для iOS по ссылке");
+            } else {
+                System.out.println("установите облегченную версию приложения для iOS по ссылке");
+            }
+        }
+        if (os == 1) {
+            if (clientDeviceYear >= 2015) {
+                System.out.println("установите версию приложения для Android по ссылке");
+            } else {
+                System.out.println("установите облегченную версию приложения для Android по ссылке");
+            }
         }
     }
 }
